5fa6c035106c4fcd61197a778e28f978d026de5d
Mona Lisa <bhamannyrat@gmail.com>
2024-10-15 21:56:05 +0530

    footer mobile responsive code update

--- src/app/Common/Dependent/Footer/index.jsx
+++ src/app/Common/Dependent/Footer/index.jsx
@@ -11,33 +11,33 @@ const Footer = () => {
     return (
         <>
             <Grid container className="py-8">
-                <Grid size={{lg:6, xs:12}}>
+                <Grid size={{md:6, xs:12}}>
                     <Typography variant="h4" color="white" gutterBottom>
                         Bharat Developer
                     </Typography>
                     <Typography variant="body1" color="#fff" gutterBottom>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
                     </Typography>
-                    <Box className="flex gap-4">
+                    <Box className="flex gap-4 justify-center md:justify-start">
                         <Facebook style={{width: "35px"}} />
                         <Instagram style={{width: "35px"}} />
                         <Linkedin style={{width: "35px"}} />
                     </Box>
                 </Grid>
-                <Grid size={{lg:6, xs:12}}>
-                    <Box className="flex justify-end gap-2" mb={1}>
+                <Grid size={{md:6, xs:12}}>
+                    <Box className="flex md:justify-end justify-center gap-2" mb={1}>
                         <HomeOutlinedIcon color='white' />
                         <Typography variant="body1" color="#fff">
                             Urlana Kalan, Panipat - 132103, India
                         </Typography>
                     </Box>
-                    <Box className="flex justify-end gap-2" mb={1}>
+                    <Box className="flex md:justify-end justify-center gap-2" mb={1}>
                         <MailOutlinedIcon color='white'/>
                         <Typography variant="body1" color="#fff">
                             contactme@bharat.com
                         </Typography>
                     </Box>
-                    <Box className="flex justify-end gap-2"  mb={1}>
+                    <Box className="flex md:justify-end justify-center gap-2"  mb={1}>
                         <CallOutlinedIcon color='white'/>
                         <Typography variant="body1" color="#fff">
                             +91 777700-4907
