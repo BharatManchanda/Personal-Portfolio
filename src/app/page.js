d14f0ab730a9f87f745433f1e33177996e8df71c
bharatmanchanda13 <bharatmanchanda13@gmail.com>
2026-01-31 12:26:33 +0000

    update

--- src/app/page.js
+++ src/app/page.js
@@ -11,13 +11,13 @@ import { useTheme } from '@mui/material/styles';
 import { api } from "./Constants/api";
 import { toast } from "react-toastify";
 import LoadingButton from '@mui/lab/LoadingButton';
-import SaveIcon from '@mui/icons-material/Save';
 import SendIcon from '@mui/icons-material/Send';
 
 export default function Home() {
 	const theme = useTheme();
 	const [contactUs, setContactUs] = useState({...initContactUs});
 	const [loading, setLoading] = useState(false);
+	const [readMore, setReadMore] = useState(false);
 	const textArray = ['Bharat Manchanda', "Laravel Developer", "React Developer", "PHP Developer", "Mern Stack Developer"];
 	
 	const [textIndex, setTextIndex] = useState(0);
@@ -113,6 +113,7 @@ export default function Home() {
 							Download CV
 						</Button>
 						<Button variant="text" color="black" className="capitalize" size="large" endIcon={<CallMadeIcon/>}
+							href="#projects"
 							sx={{ fontWeight: 'bold' }}>
 							Projects
 						</Button>
@@ -177,116 +178,124 @@ export default function Home() {
 				</Grid>
 			</Grid>
 
-			<MainHeading label={'About Me'} />
+			<Box id="about">
+				<MainHeading label={'About Me'} />
 
-			<Grid container mt={3} spacing={3}>
-				<Grid size={{lg:6, xs:12}}>
-					<Typography variant="h5" gutterBottom fontWeight={'bold'}>
-						Get to know me!
-					</Typography>
-					<Typography variant="body1" gutterBottom mb={2}>
-						I'm a <span className="font-bold">Full stack Web Developer</span> building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <span className="font-bold">Projects</span> section.
-					</Typography>
-					<Typography variant="body1" gutterBottom mb={2}>
-						I also like sharing content related to the stuff that I have learned over the years in <span className="font-bold">Web Development</span> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <span className="font-bold">Linkedin</span> and <span className="font-bold">Instagram</span> where I post useful content related to Web Development and Programming.
-					</Typography>
-					<Typography variant="body1" gutterBottom mb={2}>
-						I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.
-					</Typography>
-				</Grid>
-				<Grid size={{lg:6, xs:12}}>
-					<Typography variant="h5" gutterBottom fontWeight={'bold'}>
-						My Skills
-					</Typography>
-					<Box className="">
-						{Languages.map((coding, key) => (
-							<Button key={key} variant="contained" color="#000" sx={{
-								mr:2, mb:2,
-								fontWeight: "bold",
-								textTransform: 'capitalize',
-								background:"#D9D9D9",
-								boxShadow: "none",
-								color: "black",
-								":hover": {
-									boxShadow: "none"
-								}
-							}}>
-								{coding}
-							</Button>
-						))}
-					</Box>
-				</Grid>
-			</Grid>
-
-			<MainHeading label={'Projects'} />
-			<Grid container mt={4} spacing={3}>
-				{projects.map((project, key) => (<React.Fragment key={key}>
-					<Project project={project} count={key+1} gradientBorderColor={gradientBorderColor}/>
-				</React.Fragment>))}
-			</Grid>
-
-			<MainHeading label={'Contact Me'} />
-			<Grid container my={4} spacing={3}>
-				<Grid size={{lg:6, xs:12}}>
-					<Typography variant="h6" fontSize={'28px'} fontWeight={'bold'} gutterBottom>
-						Drop me a Message
-					</Typography>
-					<Typography variant="body1" mb={3}>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-					</Typography>
-					{contact.map((item, key) => (
-						<Box className="flex items-center gap-2 mb-2" key={key}>
-							{item.icon}
-							<Typography variant="body1" {...item.props}>
-								{item.label}
-							</Typography>
+				<Grid container mt={3} spacing={3}>
+					<Grid size={{lg:6, xs:12}}>
+						<Typography variant="h5" gutterBottom fontWeight={'bold'}>
+							Get to know me!
+						</Typography>
+						<Typography variant="body1" gutterBottom mb={2}>
+							I'm a <span className="font-bold">Full stack Web Developer</span> building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <span className="font-bold">Projects</span> section.
+						</Typography>
+						{readMore && <>
+						<Typography variant="body1" gutterBottom mb={2}>
+							I also like sharing content related to the stuff that I have learned over the years in <span className="font-bold">Web Development</span> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <span className="font-bold">Linkedin</span> and <span className="font-bold">Instagram</span> where I post useful content related to Web Development and Programming.
+						</Typography>
+						<Typography variant="body1" gutterBottom mb={2}>
+							I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.
+						</Typography></>}
+						<Button variant="contained" onClick={() => setReadMore(!readMore)}>{readMore ? 'Read Less' : 'Read More'}</Button>
+					</Grid>
+					<Grid size={{lg:6, xs:12}}>
+						<Typography variant="h5" gutterBottom fontWeight={'bold'}>
+							My Skills
+						</Typography>
+						<Box className="">
+							{Languages.map((coding, key) => (
+								<Button key={key} variant="contained" color="#000" sx={{
+									mr:2, mb:2,
+									fontWeight: "bold",
+									textTransform: 'capitalize',
+									background:"#D9D9D9",
+									boxShadow: "none",
+									color: "black",
+									":hover": {
+										boxShadow: "none"
+									}
+								}}>
+									{coding}
+								</Button>
+							))}
 						</Box>
-					))}
+					</Grid>
 				</Grid>
-				
-				<Grid size={{lg:6, xs:12}}>
-					<Paper elevation={2} component="form" onSubmit={handleContactUsSubmit} method="POST" sx={{
-						p:2,
-						".MuiFormControl-root": {
-							mb:3,
-						}
-					}} className="rounded-lg">
-						<input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
-						<TextField
-							fullWidth
-							label="Name"
-							value={contactUs.name}
-							onChange={(e) => handleContactUsChange("name", e.target.value)}
-						/>
-						<TextField
-							fullWidth
-							label="Emai"
-							value={contactUs.email}
-							onChange={(e) => handleContactUsChange("email", e.target.value)}
-						/>
-						<TextField
-							fullWidth
-							multiline rows={2}
-							label="Type Your Message"
-							value={contactUs.messsage}
-							onChange={(e) => handleContactUsChange("messsage", e.target.value)}
-						/>
-						<LoadingButton
-							type="submit"
-							loading={loading}
-							loadingPosition="start"
-							startIcon={loading ? <SendIcon /> : null}
-							variant="contained"
-							color="primary"
-							sx={{
-								textTransform: "capitalize"
-							}}
-						>
-							Send
-						</LoadingButton>
-					</Paper>
+			</Box>
+
+			<Box id="projects">
+				<MainHeading label={'Projects'} />
+				<Grid container mt={4} spacing={3}>
+					{projects.map((project, key) => (<React.Fragment key={key}>
+						<Project project={project} count={key+1} gradientBorderColor={gradientBorderColor}/>
+					</React.Fragment>))}
 				</Grid>
-			</Grid>
+			</Box>
+
+			<Box id="contact">
+				<MainHeading label={'Contact Me'} />
+				<Grid container my={4} spacing={3}>
+					<Grid size={{lg:6, xs:12}}>
+						<Typography variant="h6" fontSize={'28px'} fontWeight={'bold'} gutterBottom>
+							Drop me a Message
+						</Typography>
+						<Typography variant="body1" mb={3}>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</Typography>
+						{contact.map((item, key) => (
+							<Box className="flex items-center gap-2 mb-2" key={key}>
+								{item.icon}
+								<Typography variant="body1" {...item.props}>
+									{item.label}
+								</Typography>
+							</Box>
+						))}
+					</Grid>
+					
+					<Grid size={{lg:6, xs:12}}>
+						<Paper elevation={2} component="form" onSubmit={handleContactUsSubmit} method="POST" sx={{
+							p:2,
+							".MuiFormControl-root": {
+								mb:3,
+							}
+						}} className="rounded-lg">
+							<input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+							<TextField
+								fullWidth
+								label="Name"
+								value={contactUs.name}
+								onChange={(e) => handleContactUsChange("name", e.target.value)}
+							/>
+							<TextField
+								fullWidth
+								label="Emai"
+								value={contactUs.email}
+								onChange={(e) => handleContactUsChange("email", e.target.value)}
+							/>
+							<TextField
+								fullWidth
+								multiline rows={2}
+								label="Type Your Message"
+								value={contactUs.messsage}
+								onChange={(e) => handleContactUsChange("messsage", e.target.value)}
+							/>
+							<LoadingButton
+								type="submit"
+								loading={loading}
+								loadingPosition="start"
+								startIcon={loading ? <SendIcon /> : null}
+								variant="contained"
+								color="primary"
+								sx={{
+									textTransform: "capitalize"
+								}}
+							>
+								Send
+							</LoadingButton>
+						</Paper>
+					</Grid>
+				</Grid>
+			</Box>
 		</>
 	);
 }
